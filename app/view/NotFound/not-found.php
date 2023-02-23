128ea310b5edc3517b8406aa79b0d5825ac40035
akmmp241 <akm.mp24@gmail.com>
2023-02-23 12:17:44 +0700

    membuat css public assets dan membuat file cssConfig

---
+++ app/view/NotFound/not-found.php
@@ -0,0 +1,4 @@
+<div class="joko">
+    <h1><?= $model['error'] ?></h1>
+    <p>anda mengakses ke halaman yang tidak ada</p>
+</div>
---
+++ config/cssConfig.php
@@ -0,0 +1,6 @@
+<?php
+
+function getCSS(): string
+{
+    return "/assets/";
+}
